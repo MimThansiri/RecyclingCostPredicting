cea0fa36a5333c90c138cc3de5546b11f028033a
MimThansiri <37thansiri@gmail.com>
2024-04-01 18:53:15 +0700

    preprocess data b4 use with model, fill null 2018 with World Bank WITS web

--- Preprocess_data.py
+++ Preprocess_data.py
@@ -12,7 +12,7 @@ final_data['Date'] = pd.to_datetime(final_data['Date'])
 merged_data = pd.merge(aluminum_can, final_data, left_on='update_date', right_on='Date', how='left')
 
 # Rename the specified column
-merged_data.rename(columns={ "('ประเภทโลหะที่มีค่าสูง / Nonferrous metals', 'ราคา / หน่วย')_x": "price" }, inplace=True)
+merged_data.rename(columns={"('ประเภทโลหะที่มีค่าสูง / Nonferrous metals', 'ราคา / หน่วย')_x": "price"}, inplace=True)
 
 # Select the desired columns
 selected_columns = ['price', 'export', 'update_date']
@@ -22,8 +22,9 @@ merged_data = merged_data[selected_columns]
 # Fill 2018 missing values with 252.485
 merged_data.loc[merged_data['update_date'].dt.year == 2018, 'export'] = 252.485
 
-# Fill missing values for the year 2024 with the previous available values
-merged_data['export'] = merged_data.groupby(merged_data['update_date'].dt.year)['export'].ffill()
+# Fill missing values for the year 2024 with the last available value
+last_export_value = merged_data['export'].dropna().iloc[-1]
+merged_data.loc[merged_data['update_date'].dt.year == 2024, 'export'] = last_export_value
 
 # Save the merged data to a new CSV file with 'Date' column based on 'update_date'
 merged_data.to_csv("preprocess_data.csv", index=False)
